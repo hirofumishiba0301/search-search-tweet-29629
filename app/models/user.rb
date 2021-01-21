be28338356fa1179e8c0948116c03f9f5267a490
hirofumishiba0301 <71271771+hirofumishiba0301@users.noreply.github.com>
2021-01-22 03:11:34 +0900

    マイページ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :articles
-  has_one_attached :image
+  has_one_attached :user_image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
